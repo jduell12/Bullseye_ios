402cb6ae0cae49116f62415154f0fcae7b70280d
Jessica Duell <jessica.duell@pendo.io>
2022-02-03 17:36:12 -0500

    Completed MVP for Bullseye

--- Models/Game.swift
+++ Models/Game.swift
@@ -8,18 +8,11 @@
 import Foundation
 
 struct Game {
-    var target: Int = Int.random(in: 1...100)
-    var score: Int = 0
-    var currentRound: Int = 1
+    var target = Int.random(in: 1...100)
+    var score = 0
+    var currentRound = 1
     
     func points (sliderValue: Int) -> Int{
-        var difference: Int = self.target - sliderValue
-        
-        if difference < 0 {
-            difference *= -1
-        }
-        
-        let awardedPoints: Int = 100 - difference
-        return awardedPoints
+        100 - abs(self.target - sliderValue)
     }
 }
--- Views/ContentView.swift
+++ Views/ContentView.swift
@@ -8,9 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var alertIsVisible: Bool = false
-    @State private var sliderValue: Double = 50.0
-    @State private var game: Game = Game()
+    @State private var alertIsVisible = false
+    @State private var sliderValue = 50.0
+    @State private var game = Game()
     
     var body: some View {
         VStack {
@@ -28,20 +28,20 @@ struct ContentView: View {
                 HStack {
                     Text("1")
                         .fontWeight(.bold)
-                    Slider(value: self.$sliderValue, in: 1.0...100.0)
+                    Slider(value: $sliderValue, in: 1.0...100.0)
                     Text("100")
                         .fontWeight(.bold)
                 }
                 Button(action: {
-                    self.alertIsVisible = true
+                    alertIsVisible = true
                     
                 }) {
                     Text("Hit Me")
                 }.alert(isPresented: $alertIsVisible,
                     content: {
-                    let roundedValue: Int = Int(self.sliderValue.rounded())
+                    let roundedValue = Int(sliderValue.rounded())
                     
-                    return Alert(title: Text("The slider value is \(roundedValue)"), message: Text("You scored \(self.game.points(sliderValue: roundedValue)) points this round"), dismissButton: .default(Text("Start next Round")))
+                    return Alert(title: Text("The slider value is \(roundedValue)"), message: Text("You scored \(game.points(sliderValue: roundedValue)) points this round"), dismissButton: .default(Text("Start next Round")))
                 })
             }
         }
